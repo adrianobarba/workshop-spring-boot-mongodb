a9f065838d5925124f3f6f437d85ac008b817c96
Adriano Barbosa <adrianobarbosa_4@hotmail.com>
2021-04-28 17:22:45 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/com/adrianobarbosa/workshopmongo/resources/UserResource.java
+++ src/main/java/com/adrianobarbosa/workshopmongo/resources/UserResource.java
@@ -1,29 +1,40 @@
 package com.adrianobarbosa.workshopmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import com.adrianobarbosa.workshopmongo.domain.User;
+import com.adrianobarbosa.workshopmongo.services.UserService;
 
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 // 	@GetMapping #outra forma de anotação que substitui o (@RequestMapping)
- 	@RequestMapping(method = RequestMethod.GET)
+// 	@RequestMapping(method = RequestMethod.GET)
+//	public ResponseEntity<List<User>> findAll() {
+//      User maria = new User("1", "Maria Brown", "maria@gmail.com");
+//      User alex = new User("2", "Alex Green", "alex@gmail.com");
+//		List<User> list = new ArrayList<>();
+//		list.addAll(Arrays.asList(maria, alex));
+//		return ResponseEntity.ok().body(list);
+	
+	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		User maria = new User("1", "Maria Brown", "maria@gmail.com");
-		User alex = new User("2", "Alex Green", "alex@gmail.com");
-		List<User> list = new ArrayList<>();
-		list.addAll(Arrays.asList(maria, alex));
-		return ResponseEntity.ok().body(list);
+	List<User> list =service.findAll();
+	return ResponseEntity.ok().body(list);
+	
 	}
 
 }
---
+++ src/main/java/com/adrianobarbosa/workshopmongo/services/UserService.java
@@ -0,0 +1,20 @@
+package com.adrianobarbosa.workshopmongo.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.adrianobarbosa.workshopmongo.domain.User;
+import com.adrianobarbosa.workshopmongo.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repo;
+
+	public List<User> findAll() {
+		return repo.findAll();
+	}
+}
